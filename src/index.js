5a79e80fafe26e44cd69f9200ee99cad0689c667
Carlos Roberto <carlos.junior@dotgroup.com.br>
2022-08-09 15:48:04 -0300

    course progress part 1

---
+++ src/index.js
@@ -0,0 +1,109 @@
+// @ts-check
+
+const { caltotal } = require("./petcounter");
+
+/**
+ * Pet name
+ * @type {string}
+ */
+const petName = "Juninho";
+
+/**
+ * Number example
+ * @type {number}
+ */
+const numberExample = 100;
+
+/**
+ * My Arrayzin
+ * @type {Array<number>}
+ */
+const myArray = [10, 20, 30];
+
+/**
+ * My Arrayzin 2
+ * @type {Array<number | string | boolean>}
+ */
+const myArray2 = [10, 20, 30, "Hi", true];
+
+/**
+ * My pet Object
+ * @type {{id: number, name: string, age: number | string, gender: string}}
+ */
+const pet = {
+  id: 1,
+  name: "Juninho",
+  age: "2",
+  gender: "Male",
+};
+
+/**
+ * Calculate pet ate
+ * @param {number} current current year
+ * @param {number} yearOfBirth year of pet birth
+ * @returns {string} pet age
+ */
+const calculatePetAge = (current, yearOfBirth) => {
+  return `${current - yearOfBirth}`;
+};
+
+// console.log(calculatePetAge(2021, 2019));
+
+/**
+ * @typedef {Object} Cat
+ * @property {number} id id of cat
+ * @property {string} name name of cat
+ * @property {number | string} age age of cat
+ * @property {boolean} [isMale] gender (optional)
+ */
+
+/**
+ * @type {Cat}
+ */
+const cat = {
+  id: 1,
+  name: "Juninho",
+  age: "2",
+  isMale: true,
+};
+
+/**
+ * Class to create a new pet owner
+ */
+class Owner {
+  /**
+   * Pet owner detail
+   * @param {{name: string, age: number}} ownerDetail
+   */
+  constructor(ownerDetail) {
+    /**
+     * @property {string} name pet owner name
+     */
+    this.name = ownerDetail.name;
+    /**
+     * @property {number} age pet owner age
+     */
+    this.age = ownerDetail.age;
+  }
+
+  /**
+   * @property {Function} printOwner print out owner information
+   * @returns {void}
+   */
+  printOwner() {
+    console.log(`Owner name is ${this.name} and her age is ${this.age}.`);
+  }
+}
+
+/**
+ * Link to Owner class
+ * See {@link Owner}
+ */
+const petOwner = new Owner({
+  name: "Carlinhos",
+  age: 24,
+});
+
+petOwner.printOwner();
+
+console.log(caltotal(4, 5));
---
+++ src/petcounter.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * Pet counter module
+ * @module petcounter
+ */
+
+/**
+ * Count pet total
+ * @param {number} first existing pets
+ * @param {number} second new pets
+ * @returns {number}
+ */
+exports.caltotal = (first, second) => first + second;
